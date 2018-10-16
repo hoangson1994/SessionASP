06a07888ef8a3a030e66dfb946f13385d4d02c97
Hoang Son <sonhtd00496@fpt.edu.vn>
2018-10-16 20:46:48 +0700

    new

---
+++ DemoSessionCookie/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoSessionCookie.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DemoSessionCookie.Controllers
+{
+    public class UserController : Controller
+    {
+        public const string SessionKeyName = "_Name";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            HttpContext.Session.SetString(SessionKeyName, JsonConvert.SerializeObject(user));                                                   
+            return RedirectToAction("Info");
+        }
+
+        public IActionResult Info()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                return Redirect("~/User/Login");                
+            }
+            User user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString(SessionKeyName));
+            return View(user);
+        }
+    }
+}
---
+++ DemoSessionCookie/Data/DemoSessionCookieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoSessionCookie.Models
+{
+    public class DemoSessionCookieContext : DbContext
+    {
+        public DemoSessionCookieContext (DbContextOptions<DemoSessionCookieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoSessionCookie.Models.User> User { get; set; }
+    }
+}
